2bed1d9ee11f5dac1015a163edbdf5c0e005eb0d
kjh3165 <jhwk109@gmail.com>
2026-08-24 12:01:08 +0900

    refactor: currentStage(자취 단계) 필드 제거

--- src/main/java/com/algogyeyak/user/dto/ProfileRegisterRequest.java
+++ src/main/java/com/algogyeyak/user/dto/ProfileRegisterRequest.java
@@ -1,6 +1,5 @@
 package com.algogyeyak.user.dto;
 
-import com.algogyeyak.user.enums.CurrentStage;
 import com.algogyeyak.user.enums.TransactionType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -20,6 +19,4 @@ public class ProfileRegisterRequest {
 
     @NotNull(message = "거래 유형은 필수입니다.")
     private TransactionType transactionType;
-
-    private CurrentStage currentStage;
 }
